b997b0986ec18db0da51ee3e3c33a815d730cf65
Kays Meayan <80474123+meayan-kays@users.noreply.github.com>
2021-04-27 00:53:04 +0100

    Update MovieList.js

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -3,13 +3,13 @@ import MovieCard from './MovieCard'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { moviesData } from './MoviesData';
 
-const MovieList = ({titleSearch, rateSearch}) => {
+const MovieList = ({movies, titleSearch, rateSearch}) => {
 
     return (
         <div className="MovieList">
             <h1>List of Movies</h1>
             <div className="movies-container">
-                {moviesData
+                {movies
                 .filter(el => el.title.toLowerCase().includes(titleSearch.toLowerCase().trim())
                 && el.rating >= (rateSearch)) 
                 .map(el => ( <MovieCard {...el} key={el.title} />))}
